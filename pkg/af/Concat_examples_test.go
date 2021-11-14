f80463ed64e79caa096662db1e2340cfd697dffe
Patrick Dufour <pjdufour.dev@gmail.com>
2021-11-14 15:12:56 -0500

    go 1.17 (#5)

--- pkg/af/Concat_examples_test.go
+++ pkg/af/Concat_examples_test.go
@@ -1,6 +1,6 @@
 // =================================================================
 //
-// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Copyright (C) 2021 Spatial Current, Inc. - All Rights Reserved
 // Released as open source under the MIT License.  See LICENSE file.
 //
 // =================================================================
@@ -18,7 +18,7 @@ func Example_concat1() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fmt.Sprintf("%#v", out))
+	fmt.Printf("%#v\n", out)
 	// Output: "foobar"
 }
 
@@ -27,7 +27,7 @@ func Example_concat2() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fmt.Sprintf("%#v", out))
+	fmt.Printf("%#v\n", out)
 	// Output: "123"
 }
 
@@ -58,6 +58,6 @@ func Example_concat3() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fmt.Sprintf("%#v", out))
+	fmt.Printf("%#v\n", out)
 	// Output: "abcdefghijklmn"
 }
--- pkg/af/Flat_examples_test.go
+++ pkg/af/Flat_examples_test.go
@@ -1,6 +1,6 @@
 // =================================================================
 //
-// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Copyright (C) 2021 Spatial Current, Inc. - All Rights Reserved
 // Released as open source under the MIT License.  See LICENSE file.
 //
 // =================================================================
@@ -23,6 +23,6 @@ func Example_flat() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fmt.Sprintf("%#v", out))
+	fmt.Printf("%#v\n", out)
 	// Output: []interface {}{"a", "b", "c", "d", "e", "f"}
 }
